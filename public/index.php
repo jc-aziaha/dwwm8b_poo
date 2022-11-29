2f088a60a7c128404540f7aa00ff0ed68c12e444
Jean-Claude AZIAHA <aziaha.formations@gmail.com>
2022-11-29 16:58:49 +0100

    Bootstrap de l'application

--- public/index.php
+++ public/index.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-    </body>
-</html>
+<?php
+
+    /**
+     * ------------------------------------------------------------
+     * Le contrôleur frontal
+     * 
+     * Ses rôles :
+     *      - Réaliser le "bootstrapping" de l'application 
+     *          - Chargement de l'autoloader de composer
+     *          - Chargement des constantes
+     *          - Chargement des variables d'environnement
+     *          - Chargement du conteneur de dépendances
+     *      - Créer une nouvelle instance du noyau (kernel)
+     *          - En lui passant le conteneur en paramètres
+     *      - Demander au noyau de soumettre la requête du client au système
+     *      - Récupérer la réponse correspondante
+     *      - Envoyer cette réponse au client
+     * ------------------------------------------------------------
+     */
+
+
+    // Bootstrapping de l'application
+    require __DIR__ . "/../config/bootstrap.php";
+
+
+    dd($_ENV);
